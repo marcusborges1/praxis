21060a03bbac733c92b3cedd5f6e1e8c393fab8f
Marcus Vinícius <marcus.f22@gmail.com>
2017-06-16 08:47:54 -0300

    Notice messages translated.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to @user, notice: 'Usuário criado com sucesso.'
     else
       render :new
     end
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to @user, notice: 'Usuário atualizado com sucesso.'
     else
       render :edit
     end
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    redirect_to users_url, notice: 'Usuário foi excluído com sucesso.'
   end
 
   private
