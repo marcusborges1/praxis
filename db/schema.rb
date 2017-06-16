0571f849b4244c627f0d9efd69cc61a465cceaee
Marcus Vinícius <marcus.f22@gmail.com>
2017-06-16 09:58:20 -0300

    Sector association with users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614234446) do
+ActiveRecord::Schema.define(version: 20170616124645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170614234446) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_sectors_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,6 @@ ActiveRecord::Schema.define(version: 20170614234446) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "sectors", "users"
 end
