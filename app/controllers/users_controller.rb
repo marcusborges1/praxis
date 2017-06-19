e526b537c6e74580e4095893ff827ad6a1e3d029
renanreboredo <renanrreboredo@gmail.com>
2017-06-19 01:41:04 -0300

    Adding position association on user controller, model and necessary views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_positions, only: [:new, :edit]
 
   def index
     @users = User.all
@@ -39,11 +40,15 @@ class UsersController < ApplicationController
   end
 
   private
+    def set_positions
+      @positions = Position.all
+    end
+
     def set_user
       @user = User.find(params[:id])
     end
 
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :position_id)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  belongs_to :position, dependent: :destroy
   validates :name, presence: true
+  delegate :name, to: :position, prefix: true
 end
