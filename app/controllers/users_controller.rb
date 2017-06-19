de031996e66796ddebb39c0749a355b0f2f54332
Marcus Vinícius <marcus.f22@gmail.com>
2017-06-19 14:46:59 -0300

    Minor changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :set_positions, only: [:new, :edit]
 
   def index
     @users = User.all
@@ -40,10 +39,6 @@ class UsersController < ApplicationController
   end
 
   private
-    def set_positions
-      @positions = Position.all
-    end
-
     def set_user
       @user = User.find(params[:id])
     end
