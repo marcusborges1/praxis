928b31fd85b7604990e06d096b3d2f769215805f
renanreboredo <renanrreboredo@gmail.com>
2017-06-19 01:28:35 -0300

    Adding position migration and position to user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170614234446) do
+ActiveRecord::Schema.define(version: 20170619030038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170614234446) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "sectors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -30,7 +29,11 @@ ActiveRecord::Schema.define(version: 20170614234446) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "position_id"
+    t.index ["position_id"], name: "index_users_on_position_id", using: :btree
   end
+
+  add_foreign_key "users", "positions"
 end
