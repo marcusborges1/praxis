91be33717f090f52592ef6884972cc95dcfd65a0
Marcus Vinícius <marcus.f22@gmail.com>
2017-06-20 10:02:30 -0300

    Director added to sectors.

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,4 +1,9 @@
 class Sector < ApplicationRecord
   has_many :users
   validates :name, presence: true
+
+  def director
+    # "Diretor" will be refactored
+    users.select { |user| user.position_name == "Diretor" }.first
+  end
 end
