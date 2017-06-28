e2706e37d857d3236818e0d3646bd71c476d87d4
renanreboredo <renanrreboredo@gmail.com>
2017-06-28 16:18:00 -0300

    Deleting spec files for Evaluation Model and incrementing model spec

--- spec/controllers/evaluation_models_controller_spec.rb
+++ spec/controllers/evaluation_models_controller_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe EvaluationModelsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # EvaluationModel. As you add validations to EvaluationModel, be sure to
   # adjust the attributes here as well.
+
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
   }
@@ -43,7 +44,7 @@ RSpec.describe EvaluationModelsController, type: :controller do
 
   describe "GET #index" do
     it "returns a success response" do
-      evaluation_model = EvaluationModel.create! valid_attributes
+      EvaluationModel.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_success
     end
--- spec/models/evaluation_model_spec.rb
+++ spec/models/evaluation_model_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EvaluationModel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  xit { should validate_presence_of(position_ids:[]), skip: "Because of reasons"}
+  xit { should validate_presence_of(:name) }
+  xit { should have_many(:questions) }
 end
