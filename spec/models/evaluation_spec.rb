701bf4d713f15e0d023c3dc7d28d8c426d6ff07d
renanreboredo <renanrreboredo@gmail.com>
2017-07-02 22:46:22 -0300

    Changes on specs and in form of evaluations

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
+require 'date'
 
 RSpec.describe Evaluation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:evaluation_model) { EvaluationModel.create!(name: "Modelo 1") }
+  let(:evaluation) { Evaluation.new( start_date: Date.today, finish_date: Date.tomorrow, evaluation_model_id: evaluation_model) }
+
+  it "should have one evaluation model" do
+    association = Evaluation.reflect_on_association(:evaluation_model)
+    expect(association.macro).to eq :belongs_to
+  end
+
+  it "should validate presence of start date" do
+    evaluation.start_date = nil
+    expect(evaluation).not_to be_valid
+  end
+
+  it "should validate presence of finish date" do
+    evaluation.finish_date = nil
+    expect(evaluation).not_to be_valid
+  end
+
+  xit { should belong_to(:evaluation_model) }
+  xit { should validate_presence_of(:start_date) }
+  xit { should validate_presence_of(:finish_date) }
 end
