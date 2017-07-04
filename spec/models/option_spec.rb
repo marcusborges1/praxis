5b343f56f91fb5f061f25972316f5ff4412b3090
Marcus Vinícius <marcus.f22@gmail.com>
2017-07-04 10:48:19 -0300

    Option and Model specs refactor with shoulda matchers.

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -1,22 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Option, type: :model do
-  let(:question) {
-    Question.create(name: "foo1", description: "bar1", evaluation_factor: "baz1")
-  }
-  let(:option) { Option.new(description: "foo", question: question) }
-
-  it "is valid with valid attributes" do
-    expect(option).to be_valid
-  end
-
-  it "is not valid without a description" do
-    option.description = nil
-    expect(option).to_not be_valid
-  end
-
-  it "belongs to question" do
-    association = Option.reflect_on_association(:question)
-    expect(association.macro).to eq :belongs_to
-  end
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to belong_to(:question) }
 end
