d447a5644db63c7fda982f18acee87a95e6d5ebe
Marcus Vinícius <marcus.f22@gmail.com>
2017-07-10 11:47:34 -0300

    Custom validation to has one user at least.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ApplicationRecord
   has_many :users, through: :project_allocations
 
   validates :name, presence: true
+  validate :has_one_user_at_least
+
+  private
+  def has_one_user_at_least
+    if users.empty?
+      errors.add(:users, "need one user at least")
+    end
+  end
 end
