0773fcce605f2dd0d0462e548c4cd84df9810852
Marcus Vinícius <marcus.f22@gmail.com>
2017-07-17 15:53:43 -0300

    Context field added to Positions.

--- app/models/position.rb
+++ app/models/position.rb
@@ -3,4 +3,7 @@ class Position < ApplicationRecord
   has_many :users, through: :user_positions, dependent: :destroy
 
   validates :name, presence: true
+  validates :context, presence: true
+
+  enum context: [:institutional, :project], _prefix: true
 end
--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Position, type: :model do
   it { is_expected.to have_many(:users).through(:user_positions) }
 
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:context) }
+  it { is_expected.to define_enum_for(:context) }
 end
