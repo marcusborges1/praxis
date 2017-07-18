6bea6c44d0ae67683268b55f08746c63f402ce60
Marcus Vinícius <marcus.f22@gmail.com>
2017-07-18 11:59:42 -0300

    Added dependency on destroy and custom validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,23 @@ class User < ApplicationRecord
   has_many :projects, through: :project_allocations
 
   has_many :user_positions
-  has_many :positions, through: :user_positions
+  has_many :positions, through: :user_positions, dependent: :destroy
 
   validates :name, presence: true
 
+  validate :has_at_least_one_position
+  # validate :has_exactly_one_institutional_position
+
   delegate :name, to: :sector, prefix: true
+
+  private
+  def has_at_least_one_position
+    errors.add(:positions, "must have at least one position") if position_ids.blank?
+  end
+
+  def has_exactly_one_institutional_position
+    if positions.institutional_context.length != 1
+      errors.add(:positions, "must have exactly one institutional position")
+    end
+  end
 end
