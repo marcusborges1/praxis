8df61c152366d5608b89804c0123cf3cebfafcb1
Marcus Vinícius <marcus.f22@gmail.com>
2017-07-18 07:35:25 -0300

    Context added to permitted params.

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,25 +1,20 @@
 class PositionsController < ApplicationController
   before_action :set_position, only: [:show, :edit, :update, :destroy]
 
-  # GET /positions
   def index
     @positions = Position.all.order(:name)
   end
 
-  # GET /positions/1
   def show
   end
 
-  # GET /positions/new
   def new
     @position = Position.new
   end
 
-  # GET /positions/1/edit
   def edit
   end
 
-  # POST /positions
   def create
     @position = Position.new(position_params)
     if @position.save
@@ -29,7 +24,6 @@ class PositionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /positions/1
   def update
     if @position.update(position_params)
       redirect_to @position, notice: 'Cargo foi atualizado com sucesso'
@@ -38,7 +32,6 @@ class PositionsController < ApplicationController
     end
   end
 
-  # DELETE /positions/1
   def destroy
     @position.destroy
     redirect_to positions_url, notice: 'Cargo foi deletado com sucesso'
@@ -50,6 +43,6 @@ class PositionsController < ApplicationController
     end
 
     def position_params
-      params.require(:position).permit(:name)
+      params.require(:position).permit(:name, :context)
     end
 end
