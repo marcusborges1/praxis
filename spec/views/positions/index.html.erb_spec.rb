e264e0c508d0fac4db1541d47567b448720cb934
Marcus Vinícius <marcus.f22@gmail.com>
2017-07-18 08:46:38 -0300

    View specs included context.

--- spec/views/positions/index.html.erb_spec.rb
+++ spec/views/positions/index.html.erb_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 RSpec.describe "positions/index", type: :view do
 
   before(:each) do
-    assign(:positions, FactoryGirl.create_list(:position, 2, name: "Name"))
+    assign(:positions, [
+      FactoryGirl.create(:position, name: "Assessor", context: "institutional"),
+      FactoryGirl.create(:position, name: "Gerente", context: "project")
+      ])
   end
 
   it "renders a list of positions" do
     render
-    assert_select "tr>td", text: "Name", count: 2
+    assert_select "tr>th", text: "Nome", count: 1
+    assert_select "tr>th", text: "Tipo de Cargo", count: 1
+
+    assert_select "tr>td", text: "Assessor", count: 1
+    assert_select "tr>td", text: "Gerente", count: 1
+    assert_select "tr>td", text: "Institucional", count: 1
+    assert_select "tr>td", text: "Projeto", count: 1
   end
 end
