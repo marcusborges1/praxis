17829cd3038eac5a2cc6f8f5bb51b6ad8c21fafc
Gabriel <g.almeidaa.campos@gmail.com>
2017-07-22 19:01:09 -0300

    added authorization loading to controller

--- spec/config/controller_macros.rb
+++ spec/config/controller_macros.rb
@@ -2,9 +2,9 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      position =   FactoryGirl.create(:position, name: 'Diretor')
-      user = FactoryGirl.create(:user,)
-      user.positions << position
+      admin = FactoryGirl.create(:position, name: 'Diretor')
+      user = FactoryGirl.create(:user)
+      user.positions << admin
       sign_in user
     end
   end
