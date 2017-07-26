96a0f5e656144563a327d1c48710cc79a23ff4e7
Marcus Vinícius <marcus.f22@gmail.com>
2017-07-26 10:52:39 -0300

    All_allocated_users method created.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,11 @@ class Project < ApplicationRecord
   has_many :project_allocations
   has_many :users, through: :project_allocations
 
+  belongs_to :leader, class_name: "User"
+
   validates :name, presence: true
+
+  def all_allocated_users
+    users << leader
+  end
 end
