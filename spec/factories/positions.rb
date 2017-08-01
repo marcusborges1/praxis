63f90bb207f7e2f2981bc6eafca31792373bb008
Marcus Vinícius <marcus.f22@gmail.com>
2017-08-01 16:53:37 -0300

    Traits added to Factories.

--- spec/factories/positions.rb
+++ spec/factories/positions.rb
@@ -2,8 +2,13 @@ FactoryGirl.define do
   factory :position do
     name      { Faker::Company.profession }
     context   :institutional
+
     factory :project_position do
       context :project
+    end
+
+    trait :advisor do
+      name    "Assessor"
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,4 +7,13 @@ FactoryGirl.define do
     sector                  { FactoryGirl.create(:sector) }
     positions               { FactoryGirl.create_list(:position, 1) }
   end
+
+  trait :people_management do
+    association :sector, :people_management
+  end
+
+  trait :organizational_presidency do
+    association :sector, :organizational_presidency
+  end
+
 end
