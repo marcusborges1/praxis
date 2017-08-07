3f9078f60ec4e2db38adc6e8c0a0382fee9e3c86
Gabriel <g.almeidaa.campos@gmail.com>
2017-08-07 13:40:13 -0300

    mailer setup

---
+++ app/mailers/notifications_mailer.rb
@@ -0,0 +1,8 @@
+class NotificationsMailer < ApplicationMailer
+
+	def new_evaluation_available(user_id, evaluation_id)
+		# = url_for(host: "example.com", controller: "welcome", action: "greeting", only_path: false)
+
+	end
+
+end
