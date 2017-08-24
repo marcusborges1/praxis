6d23ffa01a7fb141ddc073d9347763b2485c1805
Gabriel <g.almeidaa.campos@gmail.com>
2017-08-24 12:49:12 -0300

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807122920) do
+ActiveRecord::Schema.define(version: 20170822234756) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,13 +38,6 @@ ActiveRecord::Schema.define(version: 20170807122920) do
     t.index ["question_value_id"], name: "index_answers_on_question_value_id", using: :btree
   end
 
-  create_table "evaluation_cycles", force: :cascade do |t|
-    t.datetime "initial_date"
-    t.datetime "end_date"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
   create_table "evaluation_models", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -59,8 +53,6 @@ ActiveRecord::Schema.define(version: 20170807122920) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "name"
-    t.integer  "evaluation_cycle_id"
-    t.index ["evaluation_cycle_id"], name: "index_evaluations_on_evaluation_cycle_id", using: :btree
     t.index ["evaluation_model_id"], name: "index_evaluations_on_evaluation_model_id", using: :btree
   end
 
@@ -158,7 +150,6 @@ ActiveRecord::Schema.define(version: 20170807122920) do
   add_foreign_key "answers", "options"
   add_foreign_key "answers", "question_values"
   add_foreign_key "evaluation_models", "sectors"
-  add_foreign_key "evaluations", "evaluation_cycles"
   add_foreign_key "evaluations", "evaluation_models"
   add_foreign_key "options", "questions"
   add_foreign_key "projects", "users", column: "leader_id"
