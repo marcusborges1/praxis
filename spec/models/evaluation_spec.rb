fe6f33e38d2b7628bffc98dc5345324044693879
Marcus Vinícius <marcus.f22@gmail.com>
2017-08-30 14:12:02 -0300

    Linked evaluations to evaluation cycle in progress at that moment.

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe Evaluation, type: :model do
   it { is_expected.to have_many(:answer_groups).dependent(:destroy) }
   it { is_expected.to belong_to(:evaluation_cycle) }
   it { is_expected.to belong_to(:evaluation_model) }
+
+  it "is linked to the current evaluation cycle in progress" do
+    current_evaluation_cycle = FactoryGirl.create(:evaluation_cycle, :in_progress)
+    evaluation = Evaluation.create(name: "oi", start_date: DateTime.now, finish_date: DateTime.tomorrow,
+                                  evaluation_model: FactoryGirl.create(:evaluation_model))
+    expect(evaluation.evaluation_cycle).to eq(current_evaluation_cycle)
+  end
 end
