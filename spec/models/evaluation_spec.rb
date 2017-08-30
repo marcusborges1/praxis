67e350281e356a5f8765a56043c23d836deb3a5c
Marcus Vinícius <marcus.f22@gmail.com>
2017-08-30 10:18:29 -0300

    Improvement in evaluation models weight on /new.

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Evaluation, type: :model do
   it { is_expected.to validate_presence_of(:finish_date) }
 
   it { is_expected.to have_many(:answer_groups).dependent(:destroy) }
-  it { is_expected.to belong_to(:evaluation_cycle) } 
+  it { is_expected.to belong_to(:evaluation_cycle) }
   it { is_expected.to belong_to(:evaluation_model) }
 end
