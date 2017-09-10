c4e16174cbd7297078dc9f8edf53398f06e5eacd
Marcus Vinícius <marcus.f22@gmail.com>
2017-09-10 17:43:21 -0300

    Access controls specs fixed.

--- spec/access_controls/user_spec.rb
+++ spec/access_controls/user_spec.rb
@@ -1,24 +1,22 @@
-require 'rails_helper'
-require 'cancan/matchers'
+require "rails_helper"
+require "cancan/matchers"
 
-RSpec.describe 'User' do
+RSpec.describe "User" do
   let(:user) {FactoryGirl.create(:user)}
   let(:answer_group) {FactoryGirl.create(:answer_group, user: user)}
   let(:another_answer_group) {FactoryGirl.create(:answer_group)}
-
 
   before(:each) do
     @ability = Ability.new(user)
   end
 
-  it 'can answer his evaluation' do
+  it "can answer his evaluation" do
     expect(@ability).to be_able_to(:read, answer_group)
     expect(@ability).to be_able_to(:update, answer_group)
   end
 
-  it 'cannot answer another user evaluation' do 
+  it "cannot answer another user evaluation" do
     expect(@ability).not_to be_able_to(:read, another_answer_group)
     expect(@ability).not_to be_able_to(:update, another_answer_group)
   end
-
 end
