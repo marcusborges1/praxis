5e42e5a98c06da0f40430f193f23c27fa91186ae
Marcus Vinícius <marcus.f22@gmail.com>
2017-10-04 23:48:28 -0300

    Removed validation of presence in evaluation comment.

--- app/models/evaluation_comment.rb
+++ app/models/evaluation_comment.rb
@@ -1,4 +1,3 @@
 class EvaluationComment < ApplicationRecord
   belongs_to :answer_group
-  validates_presence_of :comment
 end
--- spec/models/evaluation_comment_spec.rb
+++ spec/models/evaluation_comment_spec.rb
@@ -2,5 +2,4 @@ require 'rails_helper'
 
 RSpec.describe EvaluationComment, type: :model do
   it { is_expected.to belong_to(:answer_group) }
-  it { is_expected.to validate_presence_of(:comment) }
 end
