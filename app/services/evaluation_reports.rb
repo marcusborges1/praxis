2128cd4caec69bbdfc7efb55cc8910e77a217922
Marcus Vinícius <marcus.f22@gmail.com>
2017-11-08 22:51:22 -0200

    Changed individual report to native sql.

--- app/services/evaluation_reports.rb
+++ app/services/evaluation_reports.rb
@@ -1,21 +1,53 @@
 module EvaluationReports
   def self.individual_report_data(evaluation, evaluation_target_id)
-    keys = [:evaluation_factor_name, :answer_average, :question_weight, :answer_average_with_question_weight]
-
-    AnswerGroup.joins(answers: { question_value: { question: [:options, :evaluation_factor] }})
-               .where(answer_groups: { evaluation_id: evaluation.id, evaluation_target_id: evaluation_target_id, answered: true })
-               .group("evaluation_factors.name, questions.description, question_values.id")
-               .pluck("evaluation_factors.name,
-                      round(cast(avg(options.weight) as numeric), 2),
-                      question_values.value,
-                      round(cast(avg(options.weight * question_values.value) as numeric), 2)")
-               .map { |d| keys.zip(d).to_h }
+    connection = ActiveRecord::Base.connection
+    connection.execute("select
+                          ef.name evaluation_factor_name,
+                          round(cast(avg(op.weight) as numeric), 2) answer_average,
+                          qv.value question_weight,
+                          round(cast(avg(op.weight * qv.value) as numeric), 2) answer_average_with_question_weight
+                        from answer_groups ansg
+                        inner join answers ans
+                          on ans.answer_group_id = ansg.id
+                        inner join question_values qv
+                          on qv.id = ans.question_value_id
+                        inner join questions q
+                          on q.id = qv.question_id
+                        inner join options op
+                          on op.id = ans.option_id
+                        inner join evaluation_factors ef
+                          on ef.id = q.evaluation_factor_id
+                        where ansg.evaluation_id = #{connection.quote evaluation.id}
+                          and ansg.evaluation_target_id = #{connection.quote evaluation_target_id}
+                          and ansg.answered = true
+                        group by ef.name, q.description, qv.id;")
   end
 
-  def self.evaluation_final_sums(report_data)
-    {
-      average_without_weight: report_data.inject(0) { |result, e| result += e[:answer_average] },
-      average_with_weight: report_data.inject(0) { |result, e| result += e[:answer_average_with_question_weight] }
-    }
+  def self.evaluation_final_sums(evaluation, evaluation_target_id)
+    connection = ActiveRecord::Base.connection
+    connection.select_one("select
+                          	sum(report_data.answer_average) average_without_weight,
+                          	sum(report_data.answer_average_with_question_weight) average_with_weight
+                          from
+                          (select
+                            ef.name evaluation_factor_name,
+                            round(cast(avg(op.weight) as numeric), 2) answer_average,
+                            qv.value question_weight,
+                            round(cast(avg(op.weight * qv.value) as numeric), 2) answer_average_with_question_weight
+                          from answer_groups ansg
+                          inner join answers ans
+                            on ans.answer_group_id = ansg.id
+                          inner join question_values qv
+                            on qv.id = ans.question_value_id
+                          inner join questions q
+                            on q.id = qv.question_id
+                          inner join options op
+                            on op.id = ans.option_id
+                          inner join evaluation_factors ef
+                            on ef.id = q.evaluation_factor_id
+                          where ansg.evaluation_id = #{connection.quote evaluation.id}
+                            and ansg.evaluation_target_id = #{connection.quote evaluation_target_id}
+                            and ansg.answered = true
+                          group by ef.name, q.description, qv.id) report_data;")
   end
 end
